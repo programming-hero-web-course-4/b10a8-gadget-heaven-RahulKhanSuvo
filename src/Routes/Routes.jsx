7ff16c0e7f3dc3d2e326aa59cd0631bc7ac857a7
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2024-11-03 23:11:08 +0600

    initialize

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+  },
+]);
+export default router;
